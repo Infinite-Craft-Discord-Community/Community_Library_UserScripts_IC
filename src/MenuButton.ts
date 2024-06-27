e53b6de0452a63660dfad469b5f8c3c7c23b6cd1
24sanduAlexandru <andercou.alexandru@yahoo.com>
2024-06-27 13:53:28 +0300

    23th add style to actionButton

--- src/MenuButton.ts
+++ src/MenuButton.ts
@@ -45,23 +45,62 @@ buildButton(otherParent?:string)
 {
  let parentDiv=document.querySelector(otherParent??this.parentQuery);
  let buttonDiv=document.createElement("div");
+ buttonDiv.classList.add("menu-button");
  let selectionDiv=document.createElement("div");
  let selectedP=document.createElement("p");
  let optionsDiv=document.createElement("div");
+ optionsDiv.classList.add("menu-button-options");
  optionsDiv.style.overflowY="scroll";
  optionsDiv.style.height="100px";
 
 
- buttonDiv.classList.add(this.buttonClass);
+ buttonDiv.classList.add (this.buttonClass);
+
+
+ let css = `
+ .menu-button-option:hover
+     {
+       background:gray;
+
+     }
+
+.menu-button
+{
+
+scrollbar-width: none;
+
+}
+.menu-button-options {
+    overflow-y: scroll;
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none;  /* Internet Explorer 10+ */}
+ .menu-button-options::-webkit-scrollbar { /* WebKit */
+    width: 0;
+    height: 0;}
+`
+
+
+let style = document.createElement('style');
+style.appendChild(document.createTextNode(css.trim()));
+document.getElementsByTagName('head')[0].appendChild(style);
+
+
+
+
+
  
 
  this.options.forEach( (option)=>{
 
 
   let optionDiv=document.createElement("div");
+  optionDiv.classList.add("menu-button-option");
   optionDiv.appendChild(document.createTextNode(option.optionText+" "+option.optionEmoji??""));
   optionDiv.style.whiteSpace="nowrap";
   optionDiv.style.overflow="hidden";
+  optionDiv.addEventListener("mouseenter",()=>{optionDiv.style.backgroundColor="gray"});
+  optionDiv.addEventListener("mouseleave",()=>{optionDiv.style.backgroundColor="var(--background-color)"});
+
   optionDiv.addEventListener("click",()=>{
   option.handler();
   selectedP.textContent=option.optionText+" "+option.optionEmoji??"";
